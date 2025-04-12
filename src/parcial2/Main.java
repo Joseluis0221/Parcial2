b4c89c51868f1494bce3ca6e9f750f8f9ea97993
estudiante103 <estudiante103@103-17.unicamacho.co>
2025-04-11 20:54:29 -0500

    se realizo la clase main

--- src/parcial2/Main.java
+++ src/parcial2/Main.java
@@ -10,5 +10,42 @@ package parcial2;
  * @author estudiante103
  */
 public class Main {
+    public class Main {
+    public static void main (String []args) {
+        Dragon dragon = new Dragon("Draco", 100, 20);
+        Mago mago = new Mago ("Merlin", 80, 15);
+        Guerrero guerrero = new Guerrero("conan", 120, 18);
+        
+        Arma espada = new Arma ("Espada", 15);
+        Arma baston = new Arma ("Baston", 3);
+        Arma garras = new Arma ("garras", 7);
+        
+        Dragon.equiparArma(garras);
+        Mago.equiparArma(Baston);
+        Guerrero.equipoArma(espada);
+        
+        simularBatalla(dragon,mago);
+        simularBatalla(mago,guerrero);
+      
+    }
     
+    public static void 
+           simularBatalla(Criatura c1, Criatura c2) {
+               System.out.println("batalla entre " + c1.nombre + "y" + c2.nombre);
+               
+               while(c1.estaViva() && c2.estaViva()) {
+                   c1.atacar(c2);
+                   if (c2.estaViva());
+                   c2.atacar(c1);
+                   
+               }
+           }
+           if (c1.estaViva()) {
+               System.out.println(c1.nombre + "ha ganado");
+            }else{
+                   System.out.println(c2.nombre+"ha ganado");
+            }
+        }
+          
 }
+
